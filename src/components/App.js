d62b8b9d8547603ef388c1a5ad420c7a14a01d1f
gagangaur <gagan1999gaur@gmail.com>
2019-11-18 23:55:37 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import Header from "./Header";
+import Content from "./Content";
+import "../index.css";
+
+export default function App() {
+  return (
+    <Fragment>
+      <Header />
+      <Content />
+    </Fragment>
+  );
+}
